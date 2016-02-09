34f6efe267f306a2ddd35af9a7fccdfa727a7406
Bodunde Adebiyi <bodunde.adebiyi@andela.com>
2016-02-09 12:36:36 -0800

    [Fix] Remove duplicate phpunit command in travis config

--- tests/end2end/UserVideoFeatureTest.php
+++ tests/end2end/UserVideoFeatureTest.php
@@ -3,7 +3,6 @@
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-use DB;
 
 class UserVideoFeatureTest extends TestCase
 {
