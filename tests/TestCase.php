8dc9e37b8427351d0875f9aeeb6c27e39d922c09
Bodunde Adebiyi <bodunde.adebiyi@andela.com>
2016-02-09 10:07:07 -0800

    [fix] Fixing travis error modifying the configurations in phpunit.xml to fix travis errors

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -10,7 +10,7 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
      *
      * @var string
      */
-    protected $baseUrl = 'http://code.maven.com';
+    protected $baseUrl = 'http://localhost';
 
     /**
      * Creates the application.
