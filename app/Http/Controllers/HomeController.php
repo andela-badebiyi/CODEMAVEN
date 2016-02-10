219ae2f1232c8503498d0c3f38f5811a5e96d466
Bodunde Adebiyi <bodunde.adebiyi@andela.com>
2016-02-09 17:30:34 -0800

    [fix] Make Video class uppercase

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,7 +28,7 @@ class HomeController extends Controller
     public function allVideos()
     {
         //fetch all videos
-      $videos = \App\Video::all();
+      $videos = Video::all();
 
       //render view page
       return view('videos', ['videos' => $videos]);
