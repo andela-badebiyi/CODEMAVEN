ff272bdd79dd73126041be20038089c8b4036b3f
andela-badebiyi <bodunde.adebiyi@andela.com>
2016-02-17 09:22:24 -0800

    [fix] Fix the styling of 'upload video' and 'resolve request' page

--- resources/views/videorequest/request_form.blade.php
+++ resources/views/videorequest/request_form.blade.php
@@ -1,27 +1,27 @@
-@extends('layouts.master')
+@extends('layouts.dashboard')
 @section('title', 'Resolve')
 
-@section('content')
-	<div class="row con" style="margin-top:4em;">
-		@include('partials.dashboard_nav')
-		<div class=" col-xs-12 col-md-8 col-sm-12 col-lg-8" style="padding:0 3em 0 3em;">
-			<h2 style="border-bottom:solid thin #ccc; border-top:solid thin #ccc;">Resolve Request</h2>
-			<div class='panel panel-default'>
-				<div class='panel-heading'>
-					Request Info
-				</div>
-				<div class='panel-body'>
-					<strong>Requester Name:</strong> {{$request->requester_name}}<br/>
-					<strong>Requester Email:</strong> {{$request->requester_email}}<br/>
-					<strong>Request Made:</strong> {{$request->created_at->diffForHumans()}}<br/>
-					<strong>Video Description</strong> <br/>{{$request->description}}
-				</div>
-			</div>
-			@include('partials.dashboard_flash')
-			{!! Form::open(['url' => '/videos', 'method' => 'post']) !!}
-				<input type='hidden' name='request_id' value='{{$request->id}}'>
-				@include('partials.video_form')
-			{!! Form::close() !!}
+@section('details')
+	<h2 style="border-bottom:solid thin #ccc; border-top:solid thin #ccc;">Resolve Request</h2>
+	<div class='panel panel-default'>
+		<div class='panel-heading'>
+			Request Info
+		</div>
+		<div class='panel-body'>
+			<strong>Requester Name:</strong> {{$request->requester_name}}<br/>
+			<strong>Requester Email:</strong> {{$request->requester_email}}<br/>
+			<strong>Request Made:</strong> {{$request->created_at->diffForHumans()}}<br/>
+			<strong>Video Description</strong> <br/>{{$request->description}}
 		</div>
 	</div>
+	@include('partials.dashboard_flash')
+	{!! Form::open(['url' => '/videos', 'method' => 'post']) !!}
+		<input type='hidden' name='request_id' value='{{$request->id}}'>
+		@include('partials.video_form')
+	{!! Form::close() !!}
+@endsection
+
+@section('css')
+	@parent
+	<link rel='stylesheet' href='{!! asset("css/resolve.css") !!}' />
 @endsection
--- resources/views/videos/create.blade.php
+++ resources/views/videos/create.blade.php
@@ -1,20 +1,20 @@
-@extends('layouts.master')
+@extends('layouts.dashboard')
 @section('title', 'Upload')
 
-<div class="row con" style="margin-top:4em;">
-		
-	@include('partials.dashboard_nav')
-
-    <div class=" col-xs-12 col-md-8 col-sm-12 col-lg-8" style="padding:0 3em 0 3em;">
-    	@include('partials.dashboard_flash')
-        <h2 style="border-bottom:solid thin #ccc; border-top:solid thin #ccc;">
-            Upload a Video Tutorial
-        </h2>
-        <div>
-            <form action='{{ route("videos.store") }}' method='post'>
-                @include('partials.video_form')
-                
-            </form>
-        </div>
+@section('details')	
+    <h2 style="border-bottom:solid thin #ccc; border-top:solid thin #ccc;">
+        <span class='fa fa-upload'></span> Upload a Video Tutorial
+    </h2>
+    @include('partials.dashboard_flash')
+    <div>
+        <form action='{{ route("videos.store") }}' method='post'>
+            @include('partials.video_form')
+            
+        </form>
     </div>
-</div>
+@endsection
+
+@section('css')
+    @parent
+    <link rel='stylesheet' href='{!! asset("css/resolve.css") !!}' />
+@endsection
